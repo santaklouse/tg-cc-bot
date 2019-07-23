211d67bc00772f7e18ac99b464cc966d31f83356
Alex Nevpryaga <alexnevpryaga@gmail.com>
2019-07-23 22:24:53 +0300

    use /ipfs command to enable/disable logging

--- src/index.js
+++ src/index.js
@@ -18,12 +18,11 @@ let app = {
 
 let node = ipfsNode.create();
 node.once('start', (err) => {
-
+    node.id().then(data => {
+        app.log('start, node.id: ', data, data.id);
+    });
     // Lets log out the number of peers we have every 2 seconds
     setInterval(() => {
-        node.id().then(data => {
-            app.log('start, node.id: ', data, data.id);
-        });
         node.swarm.peers((err, peers) => {
             if (err) {
                 app.log('start, swarm.peers: An error occurred trying to check our peers:', err)
@@ -31,7 +30,7 @@ node.once('start', (err) => {
             }
             app.log(`start, swarm.peers: The node now has ${peers.length} peers.`)
         })
-    }, 2000);
+    }, 10000);
 
     // Log out the bandwidth stats every 4 seconds so we can see how our configuration is doing
     setInterval(() => {
@@ -54,12 +53,14 @@ node.once('start', (err) => {
 
 bot.onText(/\/ipfs/, function(msg, match) {
     chatId = msg.from.id;
+    if (chatId) {
+        chatId = null;
+    }
     // Listen for the node to start, so we can log out some metrics
     node.id().then(data => {
         // app.log(data, data.id);
         bot.sendMessage(chatId, `IPFS uniq id: <b>${data.id}</b>`, {parse_mode: 'HTML'});
     });
-
 });
 
 // process.on('beforeExit', (code) => {
