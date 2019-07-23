a314f078c14646dea8ec63ed3e33add193e0db88
Alex Nevpryaga <alexnevpryaga@gmail.com>
2019-07-24 00:17:32 +0300

    add basic  ipfs commands

--- src/index.js
+++ src/index.js
@@ -17,51 +17,75 @@ let app = {
 };
 
 let node = ipfsNode.create();
-node.once('start', (err) => {
-    node.id().then(data => {
-        app.log('start, node.id: ', data, data.id);
-    });
-    // Lets log out the number of peers we have every 2 seconds
-    setInterval(() => {
-        node.swarm.peers((err, peers) => {
-            if (err) {
-                app.log('start, swarm.peers: An error occurred trying to check our peers:', err)
-                // process.exit(1);
-            }
-            app.log(`start, swarm.peers: The node now has ${peers.length} peers.`)
-        })
-    }, 10000);
 
-    // Log out the bandwidth stats every 4 seconds so we can see how our configuration is doing
-    setInterval(() => {
-        node.stats.bw((err, stats) => {
-            if (err) {
-                app.log('stats.bw: An error occurred trying to check our stats:', err)
-            }
-            app.log(`\nBandwidth Stats: ${JSON.stringify(stats, null, 2)}\n`)
-        })
-    }, 4000)
-})
+node.once('start', (err) => {
+        node.id().then(data => {
+            app.log('start, node.id: ', data, data.id);
+        });
+    })
     .on('ready', () => {
         app.log('Node is ready to use!')
-        // your instance with WebRTC is ready
     })
     .on('error', error => {
         console.error('Something went terribly wrong!', error)
     })
     .on('stop', () => app.log('Node stopped!'));
 
-bot.onText(/\/ipfs/, function(msg, match) {
+bot.onText(/\/ipfs status/, function(msg, match) {
     chatId = msg.from.id;
     if (chatId) {
-        chatId = null;
+        return chatId = null;
     }
+    node.stats.bw((err, stats) => {
+        if (err) {
+            app.log('An error occurred trying to check our stats:', err)
+        }
+        app.log(`\nBandwidth Stats: ${JSON.stringify(stats, null, 2)}\n`)
+    })
+    node.swarm.peers((err, peers) => {
+        if (err) {
+            app.log('An error occurred trying to check our peers:', err)
+            // process.exit(1);
+        }
+        app.log(`The node now has <b>${peers.length} peers</b>.`)
+    })
+});
+
+bot.onText(/\/ipfs id/, function(msg, match) {
+    chatId = msg.from.id;
     // Listen for the node to start, so we can log out some metrics
     node.id().then(data => {
         // app.log(data, data.id);
         bot.sendMessage(chatId, `IPFS uniq id: <b>${data.id}</b>`, {parse_mode: 'HTML'});
     });
 });
+
+bot.onText(/\/ipfs stop/, function(msg, match) {
+    chatId = msg.from.id;
+    node.stop(() => {
+        app.log(`The node <b>STOPPED!</b>.`)
+    })
+});
+
+bot.onText(/\/ipfs start/, function(msg, match) {
+    chatId = msg.from.id;
+    node.start(() => {
+        app.log(`The node <b>STARTED!</b>.`)
+    })
+});
+
+bot.onText(/\/ipfs peers/, function(msg, match) {
+    chatId = msg.from.id;
+    node.swarm.peers((err, peers) => {
+        if (err) {
+            app.log('An error occurred trying to check our peers:', err)
+            // process.exit(1);
+        }
+        app.log(`The node now has <b>${peers.length} peers</b>.`)
+    })
+});
+
+
 
 // process.on('beforeExit', (code) => {
 //     console.log(`beforeExit: About to exit with code: ${code}`);
